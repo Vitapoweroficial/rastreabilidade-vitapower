0c06897b1dfeafbf96c3276ece851aa1f03a792b
Vitapoweroficial <andrew@vitapowernutrition.com.br>
2026-07-24 23:19:51 -0300

    chore: remove obsolete SQLite seed script

--- scripts/seed.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "../src/lib/db";
-import { seedDemoData } from "../src/lib/demo-data";
-
-seedDemoData(db, { reset: true });
-
-console.log("Banco SQLite populado com dados de demonstracao.");
